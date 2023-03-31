9754031752d5a9a5c238f96d77a9983503aaf9c8
PITTU PRASANTH <119800181+pittu777@users.noreply.github.com>
2023-03-31 10:21:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let weather = {
         .then((response) => {
             if (!response.ok) {
               alert("No weather ⚠️⚠️ found.");
-              throw new Error("No weather ⚠️⚠️ found.");
+              throw new Error("No weather ⚠️⚠️⚠️ found.");
             }
             return response.json();
           })
